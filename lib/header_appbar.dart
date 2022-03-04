90b0a86139bac8654c7b2a593ece3d17caad27cb
eduardolizarazu <jelo2021@gmail.com>
2022-03-04 19:31:59 -0400

    pass

--- lib/header_appbar.dart
+++ lib/header_appbar.dart
@@ -4,15 +4,31 @@ class HeaderAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      alignment: Alignment.center,
-      height: 100.0,
-      color: Colors.blue,
-      child: const Text(
-        "DESIGNERS",
-        style: TextStyle(
-            fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.bold),
+
+    const title = Text(
+      "DESIGNERS",
+      style: TextStyle(
+          fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.bold),
+    );
+
+    final menuIcon = Container(
+      margin: const EdgeInsets.only(left: 20.0),
+      child: const Icon(
+        Icons.menu,
+        color: Colors.white,
+        size: 30.0,
       ),
     );
+
+    return Container(
+        height: 100.0,
+        color: Colors.blue,
+        child: Row(
+          children: <Widget>[
+            menuIcon,
+            const SizedBox(width: 80),
+            title,
+          ],
+        ));
   }
 }
